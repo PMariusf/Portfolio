966ab1181d16043e24254aa6e8664a16efad42f8
PMariusf <p.marius.koder@gmail.com>
2024-12-16 11:50:24 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Function to toggle the visibility of the menu on small screens
-function toggleMenu() {
+f// Toggle the dropdown visibility
+function toggleDropdown() {
+    const dropdownContent = document.getElementById("myDropdown");
+    dropdownContent.classList.toggle("show");
+  }
+  
+  // Toggle the mobile navbar visibility when hamburger menu is clicked
+  function toggleMenu() {
     const navLinks = document.getElementById("navLinks");
     navLinks.classList.toggle("show");
-}
+  }
