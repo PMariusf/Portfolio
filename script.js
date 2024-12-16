d79251f9ba4c15a42e98a75006d717563280bb2d
PMariusf <p.marius.koder@gmail.com>
2024-12-16 10:39:16 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('Hello!');
+// Function to toggle the visibility of the menu on small screens
+function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("show");
+}
