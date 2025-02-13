e63962fd08de6e9238fdf8748e704ce92056a0da
PMariusf <p.marius.koder@gmail.com>
2025-02-13 14:10:45 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-function toggleMenu() {
-  document.getElementById("navLinks").classList.toggle("show");
-}
-function toggleDropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-document.addEventListener("click", function(event) {
-  if (!event.target.closest(".dropdown")) {
-      document.getElementById("myDropdown").classList.remove("show");
-  }
-  if (!event.target.closest(".hamburger") && !event.target.closest(".nav-links")) {
-      document.getElementById("navLinks").classList.remove("show");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+  });
 });
