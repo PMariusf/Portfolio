183ea8f82f0cd1f6c937dc03a5c2ffa774fc64ef
PMariusf <p.marius.koder@gmail.com>
2025-02-21 19:07:53 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menu = document.querySelector(".hamburger-menu");
-  const navLinks = document.querySelector(".nav-links");
+document.addEventListener('DOMContentLoaded', function () {
+  if (!document.querySelector('.menu-toggle')) {
+    const menuToggle = document.createElement('div');
+    menuToggle.classList.add('menu-toggle');
 
-  menu.addEventListener("click", function () {
-      navLinks.classList.toggle("show");
-  });
+    for (let i = 0; i < 3; i++) {
+      const bar = document.createElement('div');
+      menuToggle.appendChild(bar);
+    }
+
+    const navbar = document.querySelector('.navbar');
+    const navList = document.querySelector('ul');
+
+    navbar.insertBefore(menuToggle, navList);
+
+    menuToggle.addEventListener('click', function () {
+      navList.classList.toggle('active');
+      if (navList.classList.contains('active')) {
+        navList.style.position = 'absolute';
+        navList.style.right = '0';
+        navList.style.top = '100%';
+        navList.style.backgroundColor = 'rgba(18, 18, 18, 0.9)';
+        navList.style.width = '200px';
+      } else {
+        navList.removeAttribute('style');
+      }
+    });
+
+    window.addEventListener('resize', function () {
+      if (window.innerWidth > 568) {
+        navList.classList.remove('active');
+        navList.removeAttribute('style');
+      }
+    });
+  }
 });
